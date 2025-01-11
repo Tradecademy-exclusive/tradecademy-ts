ecd8d1bfed43fadf9e9bc1e8896185ce0bc6e266
Lasha Magaldadze <lashagiorgi420@gmail.com>
2025-01-11 17:17:47 +0400

    dashboard sidebar is done

---
+++ src/components/dashboard/navLink.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+interface NavLinkProps {
+  Icon: any
+  label: string
+  href: string
+}
+
+const NavLink = ({ Icon, label, href }: NavLinkProps) => {
+  const pathname = usePathname()
+  return (
+    <Link
+      href={href}
+      className={`flex items-center group gap-3 text-creme text-lg font-semibold hover:translate-x-6 transition-transform duration-300 group ${
+        pathname === href && 'translate-x-6'
+      }`}
+    >
+      {Icon}
+      <span
+        className={`group-hover:text-tcblue transition-all duration-300 ${
+          pathname === href && 'text-tcblue'
+        }`}
+      >
+        {label}
+      </span>
+    </Link>
+  )
+}
+
+export default NavLink
