8b4e27b5b51e32a3926b7dd069fd78ab80315be1
Lasha Magaldadze <lashagiorgi420@gmail.com>
2025-01-21 06:37:38 +0400

    course page is finished but now course selector needs some work

--- src/app/(routes)/dashboard/courses/[id]/Wrapper.tsx
+++ src/app/(routes)/dashboard/courses/[id]/Wrapper.tsx
@@ -8,6 +8,7 @@ import { AuthContext } from '@/providers/AuthProvider'
 import CourseSelector from '@/components/courses/courseSelector'
 import axios from 'axios'
 import WatchLesson from '@/components/courses/watchLesson'
+import Note from '@/components/courses/note'
 
 const Wrapper = ({ course }: { course: CourseType }) => {
   const searchParams = useSearchParams()
@@ -62,17 +63,119 @@ const Wrapper = ({ course }: { course: CourseType }) => {
     }
   }, [searchParams, session, course])
 
+  const goToNextLesson = () => {
+    if (session && lesson) {
+      const lessonId = searchParams.get('lesson')
+      const currentChapter = course.chapters.find((chapter) =>
+        chapter.lessons.some((lesson) => lesson.id === lessonId)
+      )
+      if (currentChapter) {
+        if (currentChapter.lessons.length <= lesson.order) {
+          return
+        } else {
+          const nextLesson = currentChapter.lessons.find((obj) => {
+            return lesson.order + 1 === obj.order
+          })
+          if (nextLesson) {
+            const newUrl = new URL(window.location.href)
+            newUrl.searchParams.set('lesson', nextLesson.id)
+            window.history.pushState({}, '', newUrl)
+          }
+        }
+      }
+    }
+  }
+
+  const goToPrevLesson = () => {
+    if (session && lesson) {
+      const lessonId = searchParams.get('lesson')
+      const currentChapter = course.chapters.find((chapter) =>
+        chapter.lessons.some((lesson) => lesson.id === lessonId)
+      )
+      if (currentChapter) {
+        if (lesson.order <= 1) {
+          return
+        } else {
+          const prevLesson = currentChapter.lessons.find((obj) => {
+            return lesson.order - 1 === obj.order
+          })
+          if (prevLesson) {
+            const newUrl = new URL(window.location.href)
+            newUrl.searchParams.set('lesson', prevLesson.id)
+            window.history.pushState({}, '', newUrl)
+          }
+        }
+      }
+    }
+  }
+
+  const checkNextDisabled = () => {
+    if (!lesson) return
+    const lessonId = searchParams.get('lesson')
+    const currentChapter = course.chapters.find((chapter) =>
+      chapter.lessons.some((lesson) => lesson.id === lessonId)
+    )
+    if (!currentChapter) {
+      return true
+    }
+    if (currentChapter.lessons.length <= lesson.order) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  const checkPrevDisabled = () => {
+    if (!lesson) return
+    const lessonId = searchParams.get('lesson')
+    const currentChapter = course.chapters.find((chapter) =>
+      chapter.lessons.some((lesson) => lesson.id === lessonId)
+    )
+    if (!currentChapter) {
+      return true
+    }
+    if (lesson.order <= 1) {
+      return true
+    } else {
+      return false
+    }
+  }
+
   if (!hasAccess) return <div></div>
   return (
     <div className='w-full flex items-start min-h-screen'>
       <div className='w-full p-6'>
         <div className='w-full flex flex-col items-center'>
-          {lesson &&
-            (lesson.source ? (
-              <WatchLesson lesson={lesson} />
-            ) : (
-              <div dangerouslySetInnerHTML={{ __html: lesson.content! }} />
-            ))}
+          <div className='w-full flex flex-col items-start gap-5'>
+            {lesson &&
+              (lesson.source ? (
+                <WatchLesson lesson={lesson} />
+              ) : (
+                <div className='w-full flex flex-col gap-5'>
+                  <div dangerouslySetInnerHTML={{ __html: lesson.content! }} />
+                  <div className='w-full flex items-center'></div>
+                </div>
+              ))}
+            <div className='w-full flex items-center justify-center gap-4'>
+              <button
+                disabled={checkPrevDisabled()}
+                onClick={goToPrevLesson}
+                className='text-sm bg-charcoal disabled:opacity-50 px-8 py-[5px] rounded-[5px] text-white'
+              >
+                Back
+              </button>
+              <button
+                disabled={checkNextDisabled()}
+                onClick={goToNextLesson}
+                className='text-sm bg-tcblue px-8 py-[5px] rounded-[5px] text-white disabled:opacity-50'
+              >
+                Next
+              </button>
+            </div>
+            <div className='w-full mt-12'>
+              {lesson?.id && <Note lessonId={lesson.id} />}
+            </div>
+          </div>
         </div>
       </div>
       <CourseSelector
--- src/components/courses/watchLesson.tsx
+++ src/components/courses/watchLesson.tsx
@@ -1,6 +1,5 @@
 import { LessonType } from '@/types'
 import PlyrIo from './plyrIo'
-import Note from './note'
 
 const WatchLesson = ({ lesson }: { lesson: LessonType }) => {
   if (!lesson.source || !lesson.type) return
@@ -11,16 +10,7 @@ const WatchLesson = ({ lesson }: { lesson: LessonType }) => {
           source={lesson.source}
           type={lesson.type.toLowerCase() as 'youtube' | 'vimeo' | 'html5'}
         />
-        <div className='w-full flex items-center justify-center gap-4'>
-          <button className='text-sm bg-charcoal px-8 py-[5px] rounded-[5px] text-white'>
-            Back
-          </button>
-          <button className='text-sm bg-tcblue px-8 py-[5px] rounded-[5px] text-white'>
-            Next
-          </button>
-        </div>
       </div>
-      <Note lessonId={lesson.id} />
     </div>
   )
 }
