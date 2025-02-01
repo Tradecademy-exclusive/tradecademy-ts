0e32f0adb9527045e9cd0683e4091d60d0d7adf9
Lasha Magaldadze <lashagiorgi420@gmail.com>
2025-02-01 22:06:11 +0400

    course lessons dropdown done finnaly

--- src/app/(routes)/admin/students/[id]/Wrapper.tsx
+++ src/app/(routes)/admin/students/[id]/Wrapper.tsx
@@ -2,7 +2,6 @@
 
 import { EnrollType, UserType } from '@/types'
 import CourseHeader from '../../components/CourseHeader'
-import Image from 'next/image'
 import { ReactNode, useState } from 'react'
 import { GraduationCap } from 'lucide-react'
 import { GrTrophy } from 'react-icons/gr'
@@ -18,6 +17,8 @@ import {
 } from '@/components/ui/table'
 import { IoIosArrowDown } from 'react-icons/io'
 import { format } from 'date-fns'
+import React from 'react'
+import { FaCheck } from 'react-icons/fa'
 
 const Card = ({
   count,
@@ -48,11 +49,13 @@ const StudentWrapper = ({
   student: UserType
   enrollments: EnrollType[]
 }) => {
-  const [openRowId, setOpenRowId] = useState('')
+  const [openRowId, setOpenRowId] = useState<string | null>(null)
+
   const lessonsByCourse = enrollments.map((enroll) => ({
     title: enroll.course.title,
     course: enroll.course,
     date: enroll.createdAt,
+    chapters: enroll.course.chapters,
     lessons: enroll.course.chapters.flatMap((chapter) => chapter.lessons || []),
   }))
 
@@ -72,35 +75,9 @@ const StudentWrapper = ({
   )
 
   return (
-    <div className='p-10 max-lg:p-5 max-md:p-4 max-sm:p-2.5 '>
+    <div className='p-10 max-lg:p-5 max-md:p-4 max-sm:p-2.5'>
       <CourseHeader page='Students' />
       <div className='mt-[200px] max-lg:mt-[280px] w-full flex flex-col items-start gap-8 border-t border-[#B9B0B0B2] p-4'>
-        <div className='w-full flex items-center gap-4'>
-          <div className='flex items-center gap-5'>
-            <div className='relative w-[60px] h-[60px] rounded-full overflow-hidden'>
-              <Image
-                src={student.picture || '/default_image.png'}
-                alt='user profile picture'
-                fill
-                className='object-cover'
-              />
-            </div>
-            <div className='flex flex-col items-start gap-0.5'>
-              <h3 className='text-lg font-bold'>{student.username}</h3>
-              <div className='flex items-center gap-5'>
-                <p className='text-[15px]'>
-                  Email: <span className='font-medium'>{student.email}</span>
-                </p>
-                <p className='text-[15px]'>
-                  Registered At:{' '}
-                  <span className='font-medium'>
-                    {student.createdAt.toDateString()}
-                  </span>
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
         <div className='w-full grid grid-cols-4 gap-5'>
           <Card
             progress='Enrolled Courses'
@@ -123,62 +100,120 @@ const StudentWrapper = ({
             icon={<IoNewspaperOutline className='text-white text-[23px]' />}
           />
         </div>
+
         <div className='w-full flex flex-col items-start gap-3.5'>
           <h4 className='text-lg font-semibold'>Course</h4>
-          <Table>
+          <Table className='border-spacing-4'>
             <TableHeader>
               <TableRow>
                 <TableHead>Course</TableHead>
                 <TableHead>Enroll Date</TableHead>
                 <TableHead>Lesson</TableHead>
-                <TableHead>Quiz</TableHead>
-                <TableHead>Assigment</TableHead>
                 <TableHead>Progress</TableHead>
+                <TableHead>Quiz</TableHead>
+                <TableHead>Assigments</TableHead>
               </TableRow>
             </TableHeader>
             <TableBody>
               {lessonsByCourse.map((course) => {
-                const completed = course.lessons.filter((lesson) => {
-                  return lesson.completed.some(
+                const completed = course.lessons.filter((lesson) =>
+                  lesson.completed.some(
                     (complete) => complete.id === student.id
                   )
-                })
+                )
                 const completePercent =
                   (completed.length / course.lessons.length) * 100
+                const isOpen = openRowId === course.course.id
+
                 return (
-                  <TableRow key={course.title} className='relative'>
-                    <TableCell className='font-medium'>
-                      {course.title}
-                    </TableCell>
-                    <TableCell className='font-medium'>
-                      {format(course.date, 'MMMM d, yyyy h:mma')}
-                    </TableCell>
-                    <TableCell className='font-medium'>
-                      {completed.length}/{course.lessons.length}
-                    </TableCell>
-                    <TableCell className='font-medium'>0/0</TableCell>
-                    <TableCell className='font-medium'>0/0</TableCell>
-                    <TableCell>
-                      <div className='flex items-center gap-3'>
-                        <div className='w-[100px] bg-[#D9D9D9] h-[6px] rounded-full flex items-start'>
-                          <div
-                            className='h-full bg-lightblue rounded-full'
-                            style={{
-                              width: `${completePercent}%`,
-                            }}
-                          />
+                  <React.Fragment key={course.course.id}>
+                    <TableRow className=''>
+                      <TableCell className='font-medium'>
+                        {course.title}
+                      </TableCell>
+                      <TableCell className='font-medium'>
+                        {format(course.date, 'MMMM d, yyyy h:mma')}
+                      </TableCell>
+                      <TableCell className='font-medium'>
+                        {completed.length}/{course.lessons.length}
+                      </TableCell>
+
+                      <TableCell className='font-medium'>0/0</TableCell>
+                      <TableCell className='font-medium'>0/0</TableCell>
+                      <TableCell>
+                        <div className='flex items-center gap-3'>
+                          <div className='w-[100px] bg-[#D9D9D9] h-[6px] rounded-full flex items-start'>
+                            <div
+                              className='h-full bg-lightblue rounded-full'
+                              style={{ width: `${completePercent}%` }}
+                            />
+                          </div>
+                          <span className='font-medium'>
+                            {completePercent || 0}%
+                          </span>
                         </div>
-                        <span className='font-medium'>
-                          {completePercent || 0}%
-                        </span>
-                      </div>
-                    </TableCell>
-                    <TableCell>
-                      <IoIosArrowDown
-                        className={`text-[#595454B2] text-xl cursor-pointer`}
-                      />
-                    </TableCell>
-                  </TableRow>
+                      </TableCell>
+                      <TableCell>
+                        <IoIosArrowDown
+                          className={`text-[#595454B2] text-xl cursor-pointer transition-transform ${
+                            isOpen ? 'rotate-180' : ''
+                          }`}
+                          onClick={() =>
+                            setOpenRowId(isOpen ? null : course.course.id)
+                          }
+                        />
+                      </TableCell>
+                    </TableRow>
+
+                    {isOpen && (
+                      <TableRow className={`bg-gray-100`}>
+                        <TableCell colSpan={7}>
+                          <div className='p-4 flex items-center justify-between gap-4'>
+                            <div className='flex flex-col items-start gap-3'>
+                              <h5 className='font-semibold'>Lessons</h5>
+                              <ul className='mt-2 space-y-1'>
+                                {course.chapters
+                                  .sort((a, b) => a.chapter - b.chapter)
+                                  .map((chapter) => {
+                                    return chapter.lessons
+                                      .sort((a, b) => a.order - b.order)
+                                      .map((lesson) => {
+                                        const isCompleted =
+                                          lesson.completed.some(
+                                            (complete) =>
+                                              complete.id === student.id
+                                          )
+                                        return (
+                                          <li
+                                            key={lesson.id}
+                                            className='flex items-center gap-2'
+                                          >
+                                            <div
+                                              className={`w-[17px] h-[17px] rounded-full flex items-center justify-center ${
+                                                isCompleted
+                                                  ? 'bg-lightblue'
+                                                  : 'bg-gray-200'
+                                              }`}
+                                            >
+                                              {isCompleted && (
+                                                <FaCheck className='text-[12px] text-white' />
+                                              )}
+                                            </div>
+                                            <span className='font-medium text-slate-700'>
+                                              {chapter.chapter}.{lesson.order}{' '}
+                                              {lesson.title}
+                                            </span>
+                                          </li>
+                                        )
+                                      })
+                                  })}
+                              </ul>
+                            </div>
+                          </div>
+                        </TableCell>
+                      </TableRow>
+                    )}
+                  </React.Fragment>
                 )
               })}
             </TableBody>
