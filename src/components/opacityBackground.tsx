8f1364489d157ac4602772ca2c5507c28bd83817
Lasha Magaldadze <lashagiorgi420@gmail.com>
2025-03-05 23:43:05 +0400

    development branch

--- src/components/opacityBackground.tsx
+++ src/components/opacityBackground.tsx
@@ -5,12 +5,13 @@ const OpacityBackground = ({
   opened: boolean
   close: () => void
 }) => {
+  console.log('bg')
   return (
     <div
       onClick={close}
       className={`fixed w-full h-screen left-0 top-0 bg-black z-[400] transition-opacity duration-300 ease-linear ${
         opened
-          ? 'opacity-80 pointer-events-auto'
+          ? 'opacity-75 pointer-events-auto'
           : 'opacity-0 pointer-events-none'
       }`}
     />
